335df68361cf4dc4889d9a46c5b3277c8ede6c49
Lassana siby <103085452+sibylassana95@users.noreply.github.com>
2022-06-11 01:25:08 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+valeur1 = int(input("Valeur 1 : "))
+valeur2 = int(input("Valeur 2 : "))
+if (valeur1 < valeur2):
+    print("Valeur la plus petite : %d " % valeur1)
+else:
+    print("Valeur la plus petite : %d" % valeur2)
